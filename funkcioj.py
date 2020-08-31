3dbb295ca2381f4a465725b4393f0b336bc71f6e
Mark Wootton <wootton.mj@gmail.com>
2020-08-31 18:52:34 +0100

    Ŝanĝoj en funkcio.py: * Ĝenerala pliordigo de la kodo * Plibonigo de la legado de komandliniaj enigoj * Aldono de dokumentado * Aldono de komentoj * Aldono de helpmesaĝo por uzanto kaj averto kiam oni uzas malfreŝajn versiojn de Python

--- funkcioj.py
+++ funkcioj.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 """
 ================================================================================
   _____    _____   _   _        _   _____   _   _   _____   _         ____
@@ -13,8 +14,9 @@ Pinjino estas sistemo por priskribi la sonojn de ĉinsignoj per latinaj literoj.
 La literoj uzataj malsamas al tiuj en la alfabeto de Esperanto, do la celo de ĉi
 tiu skripto estas konverti de norma Pinjino al proksimumo laŭ la literumsistemo
 de Esperanto. Defaŭlte, ĝi uzas la sistemon kreitan de la revuo El Popola
-Ĉinio [1], sed la uzanto povas uzi iun ajn sistemon, se ri kreas sian propran
-dosieron priskribantan ĝin. Oni rekomendas roli Pinjinon per Python 3 [2].
+Ĉinio [1], sed la uzanto povas uzi iun ajn sistemon, se oni kreas sian propran
+dosieron priskribantan ĝin. Python 3 aŭ pli freŝa versio estas bezonata por roli
+Pinjinon [2].
 
 Citaĵoj
 ----------
@@ -35,8 +37,31 @@ Aŭtorrajto
 import os
 import sys
 import string
+import textwrap
+import shutil
 
-def legiTekston(teksto, chuDosiero=False):
+# Trovi la larĝon de terminalfenestro
+try:
+    largxoDefauxlta, _ = shutil.get_terminal_size()
+except:
+    largxoDefauxlta = 80
+
+def plprint(montroto, largxo=largxoDefauxlta):
+    """
+    Funkcio por montri plurajn lineojn sen saltoj en vortoj ĉe la linefino
+
+    Enigoj
+    ----------
+    montroto : string
+        Teskto montrota
+    largxo : integer
+        Maksimuma larĝo de lineo
+        Defaŭlta valoro: "largxoDefauxlta", kiel videbla supren
+
+    """
+    print(textwrap.fill(montroto, largxo))
+
+def legiTekston(teksto):
     """
     Funkcio por havigi la tekston konverotan
 
@@ -44,8 +69,6 @@ def legiTekston(teksto, chuDosiero=False):
     ----------
     teksto : string
         Teksto konverota mem aŭ indiko al dosiero enhavanta ĝin
-    chuDosiero : boolean
-        Ĉu "teksto" estas la teksto mem aŭ ĝia dosieroindiko
 
     Eligoj
     ----------
@@ -53,15 +76,16 @@ def legiTekston(teksto, chuDosiero=False):
         La teksto konverota
 
     """
-    # if enigo is None:
-    #     return "REN Jiang nü qian QIAN" + "\n" + "āáǎà ēéěè īíǐì ōóǒò ūúǔù ǖǘǚǜ" + " " + "āáǎà ēéěè īíǐì ōóǒò ūúǔù ǖǘǚǜ".upper() + " " + "A a" + "\n"
-    # Legi tekston konverotan, se ĝi estas en dosiero,
-    if chuDosiero is True:
-        dosiero = open(os.path.join(os.getcwd(), enigo), "r")
+    # Legi la dosiero, se "teksto" estas la dosierindiko kaj ne la teksto mem
+    dosierindiko = os.path.join(os.getcwd(), teksto)
+    if os.path.isfile(dosierindiko) is True:
+        dosiero = open(dosierindiko, "r")
         teksto = ""
+        # Igi, ke "teksto" estu la enhavo de la dosiero
         for linio in dosiero:
             teksto += linio
         dosiero.close()
+    # Liveri tekston
     return teksto
 
 def legiKonvertsistemoon(ks=None):
@@ -81,15 +105,20 @@ def legiKonvertsistemoon(ks=None):
         Ordigita listo de ŝlosiloj de la vortar-objekto
 
     """
+    # Krei datumujojn
     konvertsistemo = {}
-    shlosiloj = []
+    sxlosiloj = []
+    ordo = []
     dosiero = None
     if ks is None:
+        # Uzi la defaŭlta konvertsistemon
         dosierindiko = os.path.join(os.path.split(os.path.dirname(sys.argv[0]))[0], "konvertsistemoj", "defaŭlto.csv")
     else:
         if os.path.isfile(ks):
+            # Uzi la dosieron de konvertsistemo, se "ks" estas tia
             dosierindiko = ks
         else:
+            # Serĉi konvertsistemon kun la donita nomo, se "ks" ne estas dosierindiko
             dosierujo = os.path.join(os.path.split(os.path.dirname(sys.argv[0]))[0], "konvertsistemoj")
             for dosiernomo in os.listdir(dosierujo):
                 dosierindiko = os.path.join(dosierujo, dosiernomo)
@@ -98,29 +127,36 @@ def legiKonvertsistemoon(ks=None):
                     break
                 dosiero.close()
             dosiero.close()
+    # Malfermi la dosieron de la konvertsistemo
     dosiero = open(dosierindiko, "r")
+    # Meti la konvertsistemon en vortar-objekton
     for linio in dosiero:
         aparte = linio.split(",")
+        # Trovi nomon de la konvertsistemo
         if len(aparte) == 1 and "NOMO" not in konvertsistemo:
-            konvertsistemo["NOMO"] = aparte[0][:-1]
+            konvertsistemo["NOMO"] = aparte[0].strip("\n,")
         else:
-            konvertsistemo[aparte[0]] = aparte[1][:-1]
-            shlosiloj.append(aparte[0])
-    ordo = []
-    while len(shlosiloj) > 0:
+            konvertsistemo[aparte[0].lower()] = aparte[1].strip("\n,").lower()
+            sxlosiloj.append(aparte[0].lower())
+    # Nomi ne konatan konvertsistemon
+    if "NOMO" not in konvertsistemo:
+        konvertsistemo["NOMO"] = "Ne konata konvertsistemo"
+    # Ordigi la ŝlosilojn de plej longa mallongen
+    while len(sxlosiloj) > 0:
         aldono = ""
         indekso = None
-        for shn in range(len(shlosiloj)):
-            sh = shlosiloj[shn]
-            if sh not in ordo and len(aldono) < len(sh):
-                aldono = sh
-                indekso = shn
-        ordo.append(shlosiloj.pop(indekso))
-    # print("\n", ordo, "\n")
+        for sxn in range(len(sxlosiloj)):
+            sx = sxlosiloj[sxn]
+            if sx not in ordo and len(aldono) < len(sx):
+                aldono = sx
+                indekso = sxn
+        ordo.append(sxlosiloj.pop(indekso))
+    # Fermi la dosieron de la konvertsistemo
     dosiero.close()
+    # Liveri konvertsistemon kaj ordigitajn ŝlosilojn
     return konvertsistemo, ordo
 
-def ruli(teksto, konvertsistemo, ordo):
+def konverti(teksto, konvertsistemo, ordo):
     """
     Funkcio por konverti la tekston laŭ la donita konvertsistemo
 
@@ -139,34 +175,36 @@ def ruli(teksto, konvertsistemo, ordo):
         La teksto konvertita
 
     """
+    # Krei listojn de literojn
     literoj = string.ascii_uppercase + "ĈĜĤĴŜŬ"
     kromsignoj = [["a", "āáǎà"], ["e", "ēéěè"], ["i","īíǐì"], ["o", "ōóǒò"], ["u", "ūúǔù"], ["ü", "ǖǘǚǜ"]]
+    # Krei datumujojn
     neripetoj = ""
     rezulto = ""
+    # Krei minuskligitan version de la teksto
     anstatauxigita = teksto.lower()
+    # Forigi tonajn kromsignojn
     for k in kromsignoj:
         literoj += k[1].upper()
         for kk in k[1]:
             anstatauxigita = anstatauxigita.replace(kk, k[0])
             anstatauxigita = anstatauxigita.replace(kk.upper(), k[0].upper())
+    # Meti spaceton post la linefinajn signojn
     anstatauxigita = anstatauxigita.replace("\n" , "\n ")
-    for sh in ordo:
-        anstatauxigita = anstatauxigita.replace(sh, konvertsistemo[sh].upper())
+    # Fari la anstatauxigadon laŭ la konvertsistemo
+    for sx in ordo:
+        anstatauxigita = anstatauxigita.replace(sx, konvertsistemo[sx].upper())
+    # Forigi ripetitajn literojn
     for m in range(len(anstatauxigita)-1):
         if anstatauxigita[m] != anstatauxigita[m+1] and anstatauxigita[m] in literoj:
             neripetoj += anstatauxigita[m]
         elif anstatauxigita[m] not in literoj:
             neripetoj += anstatauxigita[m]
     neripetoj += anstatauxigita[-1]
-    # print("*"*80)
-    # print(teksto)
-    # print("*"*80)
-    # print(anstatauxigita)
-    # print("*"*80)
-    # print(neripetoj)
-    # print("*"*80)
+    # Krei listojn de la individuaj vortoj de la originala kaj konverita tekstoj
     aparte = teksto.replace("\n" , "\n ").split(" ")
     neripetoj = neripetoj.split(" ")
+    # Restaŭri la usklecon de la literoj de la originala teksto en la konverita
     for tn in range(len(aparte)):
         if not len(aparte[tn]):
             continue
@@ -181,10 +219,10 @@ def ruli(teksto, konvertsistemo, ordo):
             rezulto += neripetoj[tn].lower()
         if "\n" not in neripetoj[tn]:
             rezulto += " "
-    # print(rezulto)
+    # Liveri la tekston rezultintan de la konvertado
     return rezulto
 
-def main(enigo, chuDosiero, ks):
+def main(enigo, ks):
     """
     La ĉefa funkcio
 
@@ -193,7 +231,7 @@ def main(enigo, chuDosiero, ks):
     enigo : string
         Teksto konverota mem aŭ indiko al dosiero enhavanta ĝin
     chuDosiero : boolean
-        Ĉu "enigo" estas la teksto mem aŭ ĝia dosieroindiko
+        Ĉu "enigo" estas la teksto mem aŭ ĝia dosierindiko
     ks : string
         Nomo de la konvertsistemo uzota aŭ indiko al dosiero enhavanta ĝin
 
@@ -203,66 +241,101 @@ def main(enigo, chuDosiero, ks):
         La teksto konvertita
 
     """
-    teksto = legiTekston(enigo, chuDosiero=chuDosiero)
+    teksto = legiTekston(enigo)
     konvertsistemo, ordo = legiKonvertsistemoon(ks)
-    rezulto = ruli(teksto, konvertsistemo, ordo)
-    p = "\nOriginala pinjina teksto:"
+    rezulto = konverti(teksto, konvertsistemo, ordo)
+
+    print("")
+    p = "Originala pinjina teksto:"
     if "\n" in teksto:
-        p += "\n" + teksto + "\n"
+        plprint(p)
+        p = teksto.split("\n")
+        for l in p:
+            plprint(l)
     else:
-        p += " \"%s\"\n" % teksto
-    p += "Konvertita esperantigita teksto:"
+        plprint(p + " " + teksto)
+    print("")
+    p = "Konvertita esperantigita teksto:"
     if "\n" in rezulto:
-        p += "\n" + rezulto + "\n"
+        plprint(p)
+        p = rezulto.split("\n")
+        for l in p:
+            plprint(l)
     else:
-        p += " \"%s\"\n" % rezulto
-    print(p)
+        plprint(p + " " + rezulto)
+    print("")
+
     return rezulto
+
+def helpMesagxo(versio=None):
+    """
+    Funcio por montri la helpmesaĝon, kiu priskribas:
+    * la celon de la programaro
+    * la aŭtorrajton
+    * la neprajn kaj nedevigajn enigojn laŭ ordo
+    kaj avertas la uzanton, se ties versio de Python ne estas sufiĉe freŝa.
+
+    Enigoj
+    ----------
+    La versio de Python uzata ruli la programon
+    """
+    tab = " "*4
+    print("="*80)
+    print("  _____    _____   _   _        _   _____   _   _   _____   _         ____")
+    print(" |  __ \\  |_   _| | \\ | |      | | |_   _| | \\ | | |_   _| | |       / __ \\")
+    print(" | |__) |   | |   |  \\| |      | |   | |   |  \\| |   | |   | |      | |  | |")
+    print(" |  ___/    | |   | . ` |  _   | |   | |   | . ` |   | |   | |      | |  | |")
+    print(" | |       _| |_  | |\\  | | |__| |  _| |_  | |\\  |  _| |_  | |____  | |__| |")
+    print(" |_|      |_____| |_| \\_|  \\____/  |_____| |_| \\_| |_____| |______|  \\____/")
+    print("")
+    plprint("Pinjinilo: Konverti Ĉinan Pinjinan (汉语拼音) Tekson al Esperanto-Literumsistemo")
+    print("")
+    plprint("(c) Mark Wootton 2020")
+    print("="*80)
+    print("Enigoj:")
+    plprint(tab + "[teksto] (unua, nepra)  :  la teksto konverota mem (uzu citmarkojn) aŭ indiko al tekstdosiero")
+    plprint(tab + "-k [konvertsistemo] (nedeviga)  :  dosierindiko al alternativa sistemo por konverti la tekston")
+    plprint(tab + "-h (nedeviga)  :   peti helpon - meti ĉi tiun mesaĝon kaj ĉesi")
+    if versio[0] < 3:
+        print("")
+        plprint("ATENTU: Python 3 aŭ pli freŝa versio estas bezonata. Versio %d.%d rekoniĝis." % (versio[0], versio[1]))
+    print("="*80)
 
 if __name__ == '__main__':
     """
     Ruli la ĉefan funkcion, se ĉi tiu estas la ĉefa modulo (t.e. ne alvokita)
 
     """
-    enigo = None
-    helpo = False
-    teksto = None
-    dosiero = None
-    ks = None
+    # Trovi la version de Python
+    versio = sys.version_info
+    # Krei datumujojn
+    helpo = False # Ĉu la helpmesaĝo montriĝos
+    enigo = None # La teksto por konverti
+    ks = None # Kiu konvertsistemo uziĝos
+    # Legi la komandliniajn enigojn
     argv = sys.argv[1:]
     for a in range(len(argv)):
         aa = argv[a]
-        if aa == "-h":
+        if aa.lower() in ["-h", "--helpo", "-helpo", "--help", "-help"]:
             helpo = True
-        elif aa == "-d":
-            dosiero = a+1
-        elif aa == "-t":
-            teksto = a+1
         elif aa == "-k":
             ks = a+1
-    if (dosiero is not None and teksto is not None) or (dosiero is None and teksto is None):
+    try:
+        enigo = argv[0]
+    except IndexError:
         helpo = True
-        dosiero = None
-        teksto = None
-    if dosiero is not None:
-        try:
-            enigo = argv[dosiero]
-            dosiero = True
-        except IndexError:
-            helpo = True
-    if teksto is not None:
-        try:
-            enigo = argv[teksto]
-            dosiero = False
-        except IndexError:
-            helpo = True
     if ks is not None:
         try:
             ks = argv[ks]
         except IndexError:
             helpo = True
-    if helpo:
-        print("...")
+    # Montri helpmesaĝon, se:
+    #       * neniu enigo estas donita
+    #       * la uzanto petis helpon (per "-h" ktp.)
+    #       * la enigoj ne legablas
+    #       * la versio de Python estas nesufiĉe freŝa
+    if helpo or versio[0] < 3:
+        helpMesagxo(versio)
+    # Ruli la ĉefan funkcion, se neniu problemo troviĝis
     else:
-        # print(enigo, dosiero, ks)
-        main(enigo, dosiero, ks)
+        main(enigo, ks)
